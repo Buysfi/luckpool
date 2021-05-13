a7d4316e9800ac3034ffd1ae8e7c1bf0f6266fb0
Buysfi <84071166+Buysfi@users.noreply.github.com>
2021-05-13 21:11:54 +0530

    Create setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,3 @@
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
